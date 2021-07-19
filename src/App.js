09370364ce97f4806567d123b46be85d82f2aef9
shubhamsingh11 <shubham11singh1997@gmail.com>
2021-07-19 20:18:15 +0530

    UI setup complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import { Header } from './component/Header.jsx';
+import { Balance } from './component/Balance.jsx';
+import { IncomeExpenses } from './component/IncomeExpenses.jsx';
+import { TransactionList } from './component/TransactionList.jsx';
+import { AddTransaction } from './component/AddTransaction.jsx';
 
 function App() {
   return (
     <div>
       <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
     </div>
   );
 }
