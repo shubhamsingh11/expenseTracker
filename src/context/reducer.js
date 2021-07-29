94d3bf81287b49789e6d9988bdd96527a21379b0
shubhamsingh11 <shubham11singh1997@gmail.com>
2021-07-30 02:37:49 +0530

    Added add functonality

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -3,9 +3,17 @@ export const AppReducer = (state, action) => {
         case 'DELETE_TRANSACTION':
             return {
                 ...state,
-                transactions : state.transactions.filter(transaction => transaction.id !== action.payload)
+                transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+            }
+        case 'ADD_TRANSACTION':
+            console.log(action.payload);
+            return {
+                ...state,
+                transactions: [action.payload, ...state.transactions]
             }
         default:
             return state;
     }
 }
+
+//transactions: state.transactions.push(action.transaction)
--- src/context/state.js
+++ src/context/state.js
@@ -3,11 +3,7 @@ import { AppReducer } from './reducer';
 
 //Initial State
 const initialState = {
-    transactions: [
-        { id: 1, text: 'Flower', amount: -20 },
-        { id: 2, text: 'Salary', amount: 300 },
-        { id: 3, text: 'Book', amount: -10 },
-        { id: 4, text: 'Camera', amount: 150 }]
+    transactions: []
 };
 
 //Create Global Context using createContext
@@ -25,11 +21,19 @@ export const GlobalProvider = ({ children }) => {
         })
     }
 
+    function addTransaction(transaction) {
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload : transaction
+        })
+    } 
+
     /*With the help of the below return statement we will be able to pass the transactions which is 
     part of our state to all our childern components using useContext */
     return (<GlobalContext.Provider value={{
         transactions: state.transactions,
-        deleteTransaction
+        deleteTransaction,
+        addTransaction
     }}>
         {children}
     </GlobalContext.Provider>)
