28a45ea6067abc793647831439ed7c6670538e03
shubhamsingh11 <shubham11singh1997@gmail.com>
2021-07-30 01:07:19 +0530

    Added logic for calculating income,expense and balance

--- src/component/IncomeExpenses.jsx
+++ src/component/IncomeExpenses.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React , {useContext} from 'react';
+import { GlobalContext } from '../context/state';
 
 export const IncomeExpenses = () => {
+
+  const { transactions } = useContext(GlobalContext);
+  const amount = transactions.map(transaction => transaction.amount);
+  const income = amount.filter(amount => amount > 0);
+  const expense = amount.filter(amount => amount < 0);
+  const income_amt = income.reduce((inc, item) => (inc += item), 0).toFixed(2);
+  const income_exp = expense.reduce((inc, item) => (inc += item), 0).toFixed(2);
+
     return (
         <div className="inc-exp-container">
         <div>
           <h4>Income</h4>
-          <p className="money plus">+$0.00</p>
+          <p className="money plus">+${income_amt}</p>
         </div>
         <div>
           <h4>Expense</h4>
-          <p className="money minus">-$0.00</p>
+          <p className="money minus">-${income_exp}</p>
         </div>
       </div>
     )
